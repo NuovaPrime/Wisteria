b43000b26e92602711004feb98de38aa6a642c28
Stevie <xxlennygamerxx@gmail.com>
2020-11-22 00:19:30 -0800

    Update WisteriaPlayer.Saving.cs

--- Players/WisteriaPlayer.Saving.cs
+++ Players/WisteriaPlayer.Saving.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Terraria.ModLoader;
+﻿using Terraria.ModLoader;
 using Terraria.ModLoader.IO;
 
 namespace Wisteria.Players
@@ -13,14 +8,14 @@ namespace Wisteria.Players
         public override TagCompound Save()
         {
             TagCompound tag = new TagCompound();
-            tag.Add(nameof(BreathingMastery), BreathingMastery);
-            //tag.Add(nameof(BreathingStyle), BreathingStyle);
+            tag.Add(nameof(breathingMastery), breathingMastery);
+            //tag.Add(nameof(breathingStyle), breathingStyle);
             return tag;
         }
         public override void Load(TagCompound tag)
         {
-            BreathingMastery = tag.Get<float>(nameof(BreathingMastery));
-            //BreathingStyle = tag.GetEnumerator();
+            breathingMastery = tag.Get<float>(nameof(breathingMastery));
+            //breathingStyle = tag.GetEnumerator();
         }
     }
 }
