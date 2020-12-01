44dfdb4f2a87b08f0f18912eceb998392c55e614
Mellozx <67687232+Mellozx@users.noreply.github.com>
2020-12-01 07:52:03 +0300

    slayer rank

---
+++ Players/WisteriaPlayer.SlayerRank.cs
@@ -0,0 +1,25 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace Wisteria.Players
+{
+    public partial class WisteriaPlayer : ModPlayer
+    {
+        public enum SlayerRanks
+        {
+            None,
+            Mizunoto,
+            Mizunoe,
+            Kanoto,
+            Kanoe,
+            Tsuchinoto,
+            Tsuchinoe,
+            Hinoto,
+            Hinoe,
+            Kinoto,
+            Kinoe,
+            Hashira
+        }
+        
+    }
+}
--- Wisteria.cs
+++ Wisteria.cs
@@ -1,14 +1,18 @@
+using Microsoft.Xna.Framework;
 using System.Collections.Generic;
 using Terraria;
 using Terraria.ModLoader;
 using Terraria.UI;
+using Wisteria.Players;
 using Wisteria.UI;
 using Wisteria.Utilities;
+using static Wisteria.Players.WisteriaPlayer;
 
 namespace Wisteria
 {
     public class Wisteria : Mod
     {
+        public SlayerRanks SlayerRank;
         public static Wisteria Instance { get; private set; }
 
         public static BreathUI BreathUI;
@@ -62,6 +66,28 @@ namespace Wisteria
                 },
                 InterfaceScaleType.UI)
             );
+                string SlayerRankText = $"Slayer Rank: {SlayerRank}";
+                int mouseTextIndex = layers.FindIndex(layer => layer.Name.Equals("Vanilla: Info Accessories Bar"));
+                if (mouseTextIndex != -1)
+                {
+                    layers.Insert(mouseTextIndex, new LegacyGameInterfaceLayer(
+                        "Wisteria: SlayerRank",
+                        delegate
+                        {
+                            if (Main.playerInventory == true) {
+                                Utils.DrawBorderString(
+                                Main.spriteBatch,
+                                SlayerRankText,
+                                new Vector2(Main.screenWidth - 500f, 5f),
+                                new Color(255, 255, 255)); // set a color or sum idk
+                            }
+                            return true;
+
+                        },
+                        InterfaceScaleType.UI)
+                    );
+                }
+            
         }
     }
 }
